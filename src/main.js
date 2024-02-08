af9eb0178aad6745ef0dc5b2df0d96b06eae3c38
allanteone <allanteone@gmail.com>
2024-02-08 19:47:06 +0200

    hw-25 cosmetic fix

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const dataProcessor = new DataProcessor(weatherConfig.url, weatherConfig.cities)
 async function displayTemperatures() {
     dataProcessor
         .getTemperatureData("Tel_Aviv", "2024-02-09", "2024-02-10", "22", "00")
-        .then(console.log);
+        .finally(console.log);
 }
 
 displayTemperatures();
