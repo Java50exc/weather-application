c32665c1db663d6fd1f1dc507f3aeaeadcf8e464
allanteone <allanteone@gmail.com>
2024-02-10 12:36:57 +0200

    hw-26 adds async solution

--- Deferred.mjs
+++ Deferred.mjs
@@ -1,13 +1,33 @@
+// export class Deferred {
+//     #fns = [];
+
+//     then(fn) {
+//         this.#fns.push(fn);
+//     }
+
+//     resolve(arg) {
+//         this.#fns.forEach(f => arg = f(arg));
+//     }
+// }
+
 export class Deferred {
-    #fns = [];
+    #resolveArg;
+
+    #promise = new Promise((fn) => {
+        let intID = setInterval(() => {
+            if (this.#resolveArg) {
+                clearInterval(intID);
+                fn(this.#resolveArg);
+            }
+        }, 100);
+    });
 
     then(fn) {
-        this.#fns.push(fn);
+        this.#promise = this.#promise.then(fn);
     }
 
     resolve(arg) {
-        this.#fns.forEach(f => arg = f(arg));
+        console.log(this.#promise.flag)
+        this.#resolveArg = arg;
     }
-
-
 }
